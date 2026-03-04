c0ffa5a9e25274c97d9754364d6cc2dacb073db5
Arsen <asari757@mail.ru>
2026-03-04 13:16:01 +0500

    Initial commit

---
+++ Lab5/online-store/src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface Product{
+    id:number,
+    name:string,
+    category:number,
+    description:string,
+    price:number,
+    rating:number,
+    image:string,
+    images:string[],
+    link:string,
+    likes:number,
+    isDel:boolean
+}
